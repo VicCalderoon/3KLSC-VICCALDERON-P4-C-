a6f471cf987158a2eaa2b5d5e787f1a2ddaeb0ee
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-06 15:02:06 -0700

    Agregar archivos de proyecto.

---
+++ 3KLSC-VICCALDERON-P4-C#/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    internal class Program
+    {
+        const int maxf = 3;
+        const int maxc = 5;
+        static void Main(string[] args)
+        {
+            float[,] a = new float[maxf, maxc];
+            int f, c;
+
+            for (f = 0; f < maxf; f++)
+            {
+                for (c = 0; c < maxc; c++)
+                {
+                    a[f, c] = float.Parse(Console.ReadLine());
+                }
+            }
+
+            for (f = 0; f < maxf; f++)
+            {
+                for (c = 0; c < maxc; c++)
+                {
+                    Console.Write(a[f, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+
+
+
+
+}
